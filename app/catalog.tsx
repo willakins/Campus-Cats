4b17a84f83360308feb870eac252ed209fe370fb
willakins <willakins23@gmail.com>
2024-11-15 10:50:02 -0500

    Adding basic navbar

---
+++ app/catalog.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Catalog() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Catalog Screen</Text>
+    </View>
+  );
+}
