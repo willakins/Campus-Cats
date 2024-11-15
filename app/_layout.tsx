8c90585347148f87cb1aae35f01dba3041e5d5f1
willakins <willakins23@gmail.com>
2024-11-15 10:50:02 -0500

    Adding basic navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,62 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        headerShown: false, // Hide the default header
+        tabBarStyle: { backgroundColor: 'white', height: 60 },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      <Tabs.Screen
+        name="index" // Maps to the `index.tsx` file - this is the map of GTech
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="announcements" // Maps to the announcements.tsx file
+        options={{
+          tabBarLabel: 'Announcements',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="picture" // Maps to the `picture.tsx` file
+        options={{
+          tabBarLabel: 'Take Picture',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat" // Maps to the `chat.tsx` file
+        options={{
+          tabBarLabel: 'Take Picture',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat" // Maps to the `catalog.tsx` file
+        options={{
+          tabBarLabel: 'Catalog',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text> Put an apple maps here</Text>
     </View>
   );
 }
