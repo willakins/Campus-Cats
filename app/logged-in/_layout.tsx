f5c6e99da62475fefe40f60005959e01dfc3561f
willakins <willakins23@gmail.com>
2025-02-08 13:55:48 -0500

    Add settings screen and added logout feature

--- app/logged-in/_layout.tsx
+++ app/logged-in/_layout.tsx
@@ -1,63 +1,106 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Stack } from "expo-router";
 import { Tabs } from 'expo-router';
+import React from "react";
+import { TouchableOpacity, View, StyleSheet, Text, StatusBar } from "react-native";
+import { Link, useRouter } from 'expo-router';
 
 export default function RootLayout() {
-  const size = 29;
+  const size2 = 29;
+  const color2 = '#333'
+
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false, // Hide the default header
-        tabBarStyle: { backgroundColor: 'white', height: 90 },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      }}
-    >
-      <Tabs.Screen
-        name="index" // Maps to the `index.tsx` file - this is the map of GTech
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="location-outline" size={29} color={color} />
-          ),
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#333" />
+      <View style={styles.tabs}>
+        <Tabs
+        screenOptions={{
+          headerShown: false, // Hide the default header
+          tabBarStyle: { backgroundColor: 'white', height: 0 },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
         }}
-      />
-      <Tabs.Screen
-        name="announcements" // Maps to the announcements.tsx file
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="notifications-outline" size={29} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="logout" // Used to map to the `picture.tsx` file, now logs user out
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="log-out" size={29} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="chat" // Maps to the `chat.tsx` file
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="chatbox-outline" size={29} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="catalog" // Maps to the `catalog.tsx` file
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="image-outline" size={29} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+        >
+          <Tabs.Screen
+            name="index" // Maps to the `index.tsx` file - this is the map of GTech
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="location-outline" size={size2} color={color2} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="announcements" // Maps to the announcements.tsx file
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="notifications-outline" size={size2} color={color2} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="chat" // Maps to the `chat.tsx` file
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="chatbox-outline" size={size2} color={color2} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="catalog" // Maps to the `catalog.tsx` file
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="image-outline" size={size2} color={color2} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="settings" // Maps to the `catalog.tsx` file
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="settings-outline" size={size2} color={color2} />
+              ),
+            }}
+          />
+        </Tabs>
+      </View>
+    </View>
+    
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end', // Tab navigator will sit at the bottom
+  },
+  screenContainer: {
+    flex: 1,
+    top: -50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoutButton: {
+    position: 'absolute',
+    top: -10,
+    left: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#333',
+    padding: 5,
+    borderRadius: 5,
+    zIndex: 10, // Ensure the logout button is on top
+  },
+  logoutText: {
+    color: '#fff',
+    marginLeft: 5,
+  },
+  tabs: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    bottom: 0,
+    backgroundColor: '#333',
+  }
+});
---
+++ app/logged-in/settings.tsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { View, Text, ActivityIndicator, TouchableOpacity} from 'react-native';
+import { StyleSheet } from 'react-native';
+import { doc, getDoc } from 'firebase/firestore';
+import { auth, db } from './firebase';
+import { useRouter } from 'expo-router';
+
+const Settings = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+    const router = useRouter();
+    const handleLogout = () => {
+      router.push('/login')
+    };
+    useEffect(() => {
+        const checkUserRole = async () => {
+        try {
+            // Get current user ID
+            const user = auth.currentUser;
+            if (!user) {
+            console.error('User not logged in');
+            setLoading(false);
+            return;
+            }
+
+            // Fetch user role from Firestore
+            const userDoc = await getDoc(doc(db, 'users', user.uid));
+            if (userDoc.exists()) {
+            const { role } = userDoc.data();
+            setIsAdmin(role === 1 || role === 2); // Admin roles are 1 or 2
+            } else {
+            console.error('User document does not exist');
+            }
+        } catch (error) {
+            console.error('Error fetching user role:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    checkUserRole();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.screen}>
+        <ActivityIndicator size="large" color="#000" />
+      </View>
+    );
+  }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+            <Ionicons name="log-out-outline" size={25} color="#fff" />
+        </TouchableOpacity>
+        <Text>Setting Screen</Text>
+        {isAdmin ? (
+                <Ionicons
+                  name="lock-closed"
+                  size={24}
+                  color="black"
+                  style={styles.lockIcon}
+                />
+              ) : null}
+    </View>
+  );
+}
+
+export default Settings;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      lockIcon: {
+        marginRight: 15, // Adjust as needed for positioning
+      },
+    container: {
+      flex: 1,
+      justifyContent: 'flex-end', // Tab navigator will sit at the bottom
+    },
+    screenContainer: {
+      flex: 1,
+      top: -50,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logoutButton: {
+      position: 'absolute',
+      top: -10,
+      left: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#333',
+      padding: 5,
+      borderRadius: 5,
+      zIndex: 10, // Ensure the logout button is on top
+    },
+    logoutText: {
+      color: '#fff',
+      marginLeft: 5,
+    },
+    tabs: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      bottom: 0,
+      backgroundColor: '#333',
+    }
+  });
