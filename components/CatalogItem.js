2137b3236f6d1d75955753daf25497ce1c8f1111
willakins <willakins23@gmail.com>
2025-02-21 13:46:14 -0500

    Adding boiler files

--- components/CatalogItem.js
+++ components/CatalogItem.js
@@ -1,25 +1,13 @@
-// CatalogEntry.js
+// CatalogItem.js
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from "react-native-maps";
 
-const CatalogEntry = ({ name, profilePhoto, info, recentSighting, extraPhotos  }) => {
+const CatalogItem = ({ name, profilePhoto, info }) => {
   return (
     <View style={styles.entryContainer}>
         <Text style={styles.title}>{name}</Text>
         <Image source={{ uri: profilePhoto }} style={styles.image} />
         <Text style={styles.description}>{info}</Text>
-        <MapView
-            style={{ width: '100%', height: 200, marginVertical: 10 }}
-            initialRegion={{
-              latitude: 33.7756, // Default location (e.g., Georgia Tech)
-              longitude: -84.3963,
-              latitudeDelta: 0.01,
-              longitudeDelta: 0.01,
-            }}
-          >
-            {recentSighting && <Marker coordinate={recentSighting} />}
-        </MapView>
     </View>
   );
 };
@@ -49,4 +37,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CatalogEntry;
+export default CatalogItem;
