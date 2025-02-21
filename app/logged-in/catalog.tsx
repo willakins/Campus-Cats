c812789134e4d4dcc559dcfa4865716d8dc13462
willakins <willakins23@gmail.com>
2025-02-21 14:39:48 -0500

    Add ability to open catalog entry

--- app/logged-in/catalog.tsx
+++ app/logged-in/catalog.tsx
@@ -1,19 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 import { db, storage } from "./firebase"; // Import your firebase config
-import { FlatList, Text, StyleSheet, View, Image, ScrollView } from 'react-native';
-import CatalogItem from '../../components/CatalogEntry';
+import { FlatList, Text, StyleSheet, View, Image, ScrollView, SafeAreaView } from 'react-native';
+import CatalogItem from '../../components/CatalogItem';
 import { collection, getDocs } from "firebase/firestore";
-
-interface CatalogEntryObject {
-  id: string;
-  name: string;
-  profilePhoto: string; //path to storage with photo
-  info: string;
-  most_recent_sighting: string; // document id for a cat sighting 
-  extraPhotos: string; //path to a storage folder with other photos
-  
-}
+import { CatalogEntryObject } from "@/types/CatalogEntryObject";
 
 export default function Catalog() {
   const [catalogEntries, setCatalogEntries] = useState<CatalogEntryObject[]>([]);
@@ -40,57 +31,46 @@ export default function Catalog() {
   }, []);
 
   return (
-    <ScrollView style={styles.scrollView}>
-      {catalogEntries.map((entry) => (
-        <CatalogItem
-          key={entry.id}
-          name={entry.name}
-          profilePhoto={entry.profilePhoto}
-          info={entry.info}/>
-      ))}
-    </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Catalog</Text>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        {catalogEntries.map((entry) => (
+          <CatalogItem
+            id={entry.id}
+            name={entry.name}
+            profilePhoto={entry.profilePhoto}
+            info={entry.info}
+            most_recent_sighting={entry.most_recent_sighting}
+            extraPhotos={entry.extraPhotos}
+            />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+    
   );
 }
 const styles = StyleSheet.create({
-  scrollView: {
+  container: {
     flex: 1,
+    backgroundColor: '#F4F4F9',
     paddingTop: 20,
+    paddingHorizontal: 15,
   },
-    list: {
-      padding: 16,
-    },
-    item: {
-      marginBottom: 16,
-      padding: 20,
-      backgroundColor: '#fff',
-      borderRadius: 8,
-      shadowColor: '#000',
-      shadowOpacity: 0.1,
-      shadowOffset: { width: 0, height: 2 },
-      shadowRadius: 4,
-      elevation: 3,
-      flexDirection: 'row', // Align items horizontally
-      alignItems: "center",
-      justifyContent: 'center',
-    },
-    photoContainer: {
-      flexDirection: 'row', // Align items horizontally
-      marginBottom: 15,
-      alignItems: "center",
-      justifyContent: 'center',
-    },
-    image: {
-      width: '50%', // Take up the full width of the container
-      aspectRatio: 16 / 9, // Maintain a 16:9 aspect ratio
-      borderRadius: 10,
-      marginRight: 10,
-    },
-    container: {
-      flexDirection: 'row', // Align items horizontally
-      alignItems: 'center', // Vertically center the image and text
-    },
-    text: {
-      fontSize: 15,
-      marginRight: 50,
-    },
-  });
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 20,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  scrollView: {
+    padding: 10,
+  },
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F4F4F9',
+  },
+});
