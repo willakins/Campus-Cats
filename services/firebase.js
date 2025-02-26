3f9344bd219bf757449c571633b2b6ff0ba7322e
Robert Gu <arcwandx86@gmail.com>
2025-02-26 04:26:53 -0500

    Use env file
    Note that the EXPO_PUBLIC variables are not actually secret for Firebase
    but it's still generally good practice

--- services/firebase.js
+++ services/firebase.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqweo_-BDd8jz-h0n_cKk0jgMbLu9w4kg",
-  authDomain: "campuscats-d7a5e.firebaseapp.com",
-  projectId: "campuscats-d7a5e",
-  storageBucket: "campuscats-d7a5e.firebasestorage.app",
-  messagingSenderId: "488622327541",
-  appId: "1:488622327541:web:6d94f6cb1532efc08ce7bc",
-  measurementId: "G-WM410CRM8T"
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
