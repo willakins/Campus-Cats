4858bf8abda34085e0b8a5ddf2a02571e42cd4b8
Robert Gu <arcwandx86@gmail.com>
2025-02-27 13:46:00 -0500

    Hide screens while authentication is loading

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,14 @@
 import { Redirect, Stack } from 'expo-router';
 
 import { useAuth } from '@/providers';
+import { LoadingIndicator } from '@/components';
 
 const AuthLayout = () => {
-  const { currentUser } = useAuth();
+  const { currentUser, loading } = useAuth();
+
+  if (loading) {
+    return <LoadingIndicator />;
+  }
 
   if (currentUser) {
     // If we are already logged in, bypass login screens
