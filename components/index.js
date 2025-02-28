921f3d51b9487c638845df12f2f6fea639ab2473
Robert Gu <arcwandx86@gmail.com>
2025-02-27 19:00:34 -0500

    Increase robustness of authentication

--- components/index.js
+++ components/index.js
@@ -1,9 +1,9 @@
 export { Button, BorderlessButton } from './ui/Buttons';
 export { TextInput } from './ui/TextInput';
+export { LoadingIndicator } from './ui/LoadingIndicator';
 
 export { SightingMapView } from './SightingMapView'
 
 export { CatalogEntry } from './CatalogEntry';
 export { CatalogItem } from './CatalogItem';
 export { Login } from './Login';
-export { LoadingIndicator } from './LoadingIndicator';
