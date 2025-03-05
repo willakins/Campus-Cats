496a1164489f3b9bec762d9f2a811d158599ff21
Robert Gu <arcwandx86@gmail.com>
2025-03-05 08:08:21 -0500

    Set up User models
    Don't use react-query just yet

---
+++ models/User.ts
@@ -0,0 +1,25 @@
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { z } from 'zod';
+
+import { db } from '@/config/firebase';
+
+const User = z.object({
+  id: z.string(),
+  email: z.string().email(),
+  role: z.number().int().default(0),
+});
+
+type User = z.infer<typeof User>;
+
+const path = 'users';
+
+const fetchUser = async (id: string) => {
+  const userDoc = await getDoc(doc(db, path, id));
+  return User.parse({ id: userDoc.id, ...userDoc.data() });
+};
+
+const mutateUser = async ({id, ...data}: User) => {
+  await setDoc(doc(db, path, id), data);
+};
+
+export { User, fetchUser, mutateUser }
