e3f7390be2011133a7f3bb7eed973950612c2ebc
Robert Gu <arcwandx86@gmail.com>
2025-03-08 12:14:17 -0500

    Switch to react-hook-form for Login

--- components/forms/Login.tsx
+++ components/forms/Login.tsx
@@ -1,31 +1,49 @@
 import { useState } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
-import { Button, BorderlessButton } from './ui/Buttons';
-import { TextInput } from './ui/TextInput';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button, BorderlessButton } from '@/components/ui/Buttons';
+import { ControlledInput } from '@/components/ui/TextInput';
+
+// Login requirements
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type FormType = z.infer<typeof schema>;
 
 type LoginProps = {
-  onSubmit: (user: string, pass: string) => Promise<string>;
-  onCreateAccount?: (... args: any[]) => any;
-  onBack?: (... args: any[]) => any;
+  onSubmit: (email: string, pass: string) => any;
+  type: 'login' | 'createAccount';
+  onSwitchType?: (... args: any[]) => any;
   forgotPassword?: boolean;
 };
 
-export const Login: React.FC<LoginProps> = ({
+export const LoginForm: React.FC<LoginProps> = ({
   onSubmit,
-  onCreateAccount,
-  onBack,
+  type,
+  onSwitchType,
   forgotPassword = false
 }) => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
 
+  const { handleSubmit, control } = useForm<FormType>({
+    resolver: zodResolver(schema),
+  });
+
   // Wrap submit function to convert the Promise<String> into just a string
-  const handleSubmit = async () => {
-    const err = await onSubmit(username, password);
-    if (err) {
-      setError(err);
+  const submitHandler = async (data: FormType) => {
+    try {
+      await onSubmit(data.email, data.password);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        // TODO: Give proper error messages
+        setError(err.message);
+      }
     }
   };
 
@@ -33,23 +51,16 @@ export const Login: React.FC<LoginProps> = ({
     <View style={styles.container}>
       <Image source={require('@/assets/images/campus_cats_logo.png')} style={styles.logo} resizeMode='contain'/>
       <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Email" 
-          onChangeText={setUsername}
-          keyboardType="email-address" />
-        <TextInput 
-          placeholder="Password" 
-          onChangeText={setPassword}
-          secureTextEntry />
-        <Button onPress={handleSubmit}>
-          {onCreateAccount !== undefined ? 'Sign In' : 'Create Account'}
+        <ControlledInput control={control} name="email" label="Email" />
+        <ControlledInput control={control} name="password" label="Password" secureTextEntry />
+        <Button onPress={handleSubmit(submitHandler)}>
+          {type === 'login' ? 'Sign In' : 'Create Account'}
         </Button>
-        {onCreateAccount !== undefined ?
-          <Button onPress={onCreateAccount}>
-            Create Account
+        {onSwitchType !== undefined ?
+          <Button onPress={onSwitchType}>
+            {type === 'login' ? 'Create Account' : 'Go Back'}
           </Button>
         : null}
-        {onBack !== undefined ? <Button onPress={onBack}>Go Back</Button> : null}
         {forgotPassword ?
           <BorderlessButton
             onPress={() => alert("Contact an Administrator")}
