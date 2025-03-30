74ec3a3ec10e7b357fafec841dae953e2f846480
Robert Gu <arcwandx86@gmail.com>
2025-03-30 11:50:40 -0400

    Fix padding by changing safearea on iOS
    also fix indentation

--- styles/global.js
+++ styles/global.js
@@ -1,35 +1,38 @@
 //Common styles
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 const globalStyles = StyleSheet.create({
-    tabs: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      backgroundColor: '#333',
-    },
-    lockIcon: {
-      position: 'absolute',
-      top: 10,
-      right: 20,
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 5,
-      borderRadius: 5,
-      zIndex: 10,
-    },
-    homeScreen: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-    screen: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    safeView: {
-      flex: 1,
-      backgroundColor: "#ccc", // Set the notch area color
-      marginBottom:-40,
-    },
+  tabs: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: '#333',
+  },
+  lockIcon: {
+    position: 'absolute',
+    top: 10,
+    right: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 5,
+    borderRadius: 5,
+    zIndex: 10,
+  },
+  homeScreen: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  safeView: { // Controls the SafeAreaView over the entire app
+    flex: 1,
+    backgroundColor: "#ccc", // Set the notch area color
+    marginBottom: Platform.select({
+      ios: -40, // avoid iOS extra space on bottom
+      default: 0
+    }),
+  },
 });
 export { globalStyles };
