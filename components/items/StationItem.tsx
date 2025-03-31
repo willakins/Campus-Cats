19fd1eb28b45f41a22710c00914d0dbc5a8a5ef6
willakins <willakins23@gmail.com>
2025-03-30 22:52:35 -0400

    Removed bad white space, unused imports, and unused passed parameters

--- components/items/StationItem.tsx
+++ components/items/StationItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Image, Text, View } from 'react-native';
 
 import { useFocusEffect, useRouter } from 'expo-router';
