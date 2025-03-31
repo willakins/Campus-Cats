49522b9b8e156d568db14bea16831e225fc49912
willakins <willakins23@gmail.com>
2025-03-30 22:46:45 -0400

    Fixed minor merge error

--- components/items/StationItem.tsx
+++ components/items/StationItem.tsx
@@ -6,7 +6,6 @@ import { Button } from '../ui/Buttons';
 import { StationEntryObject } from '@/types';
 import DatabaseService from '../services/DatabaseService';
 import { Checkbox } from "react-native-paper";
-import { Checkbox } from "react-native-paper";
 import { globalStyles, buttonStyles, textStyles, containerStyles } from '@/styles';
 
 export const StationItem: React.FC<StationEntryObject> = ({ id, name, longitude, latitude, lastStocked, stockingFreq, 
@@ -30,8 +29,6 @@ export const StationItem: React.FC<StationEntryObject> = ({ id, name, longitude,
         <Text style={textStyles.catalogTitle}>{name}</Text>
         <View style={containerStyles.stationsEntry}>
           {profileURL ? (<Image source={{ uri: profileURL }} style={containerStyles.listImage2} />) : 
-        <View style={containerStyles.stationsEntry}>
-          {profileURL ? (<Image source={{ uri: profileURL }} style={containerStyles.listImage2} />) : 
           <Text style={textStyles.title}>Loading image...</Text>}
           <View style={containerStyles.stockContainer}>
             <Text style={[textStyles.normalText, { color: isStocked ? "green" : "red"}]}>
@@ -45,7 +42,6 @@ export const StationItem: React.FC<StationEntryObject> = ({ id, name, longitude,
             </View>
           </View>
         </View> 
-        
         <Text style={textStyles.catalogDescription}>Known Cats: {knownCats}</Text>
       </View>
     </Button>
