74a57b135be3abf15089a43e1d92524564d96f81
Robert Gu <arcwandx86@gmail.com>
2025-04-04 11:21:13 -0400

    Refactor to use Sighting form

--- app/(app)/sighting/edit-report.tsx
+++ app/(app)/sighting/edit-report.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Image, Text, KeyboardAvoidingView, Platform, ScrollView, Switch, View } from 'react-native';
+import { Image, Text, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import MapView, { LatLng, Marker } from 'react-native-maps';
 
-import { Button, TextInput } from '@/components';
+import { Button, Switch, TextInput } from '@/components';
 import DatabaseService from '@/components/services/DatabaseService';
 import { Ionicons } from '@expo/vector-icons';
 import { buttonStyles, textStyles, containerStyles } from '@/styles';
+import { SightingReportForm } from '@/forms';
+import { Sighting } from '@/models';
 import { CatSightingObject } from '@/types';
 
 const CatSightingScreen = () => {
@@ -16,41 +18,41 @@ const CatSightingScreen = () => {
 
 
   const docRef:string = docId as string;
-  const date = new Date(JSON.parse(catDate as string));
-  const [fed, setFed] = useState<boolean>(JSON.parse(catFed as string));
-  const [health, setHealth] = useState<boolean>(JSON.parse(catHealth as string));
+  const spotted_time = new Date(JSON.parse(catDate as string));
+  const fed = JSON.parse(catFed as string);
+  const health = JSON.parse(catHealth as string);
   const photoUrl = catPhoto as string;
-  const [info, setInfo] = useState<string>(catInfo as string);
-  const [longitude, setLongitude] = useState<number>(parseFloat(catLongitude as string));
-  const [latitude, setLatitude] = useState<number>(parseFloat(catLatitude as string));
-  const [name, setName] = useState<string>(catName as string);
+  const info = catInfo as string;
+  const longitude = parseFloat(catLongitude as string);
+  const latitude = parseFloat(catLatitude as string);
+  const name = catName as string;
   const [photoImage, setPhoto] = useState<string>('');
   const database = DatabaseService.getInstance();
-  const thisSighting = new CatSightingObject(docRef, name, info, photoUrl, fed, health, date, latitude, longitude);
-
-  var location:LatLng = {
-    latitude: latitude,
-    longitude: longitude,
-  };
+  const thisSighting: Sighting = {id: docRef, name, info, image: photoUrl, fed, health, spotted_time, latitude, longitude};
 
   useEffect(() => {
     database.fetchImage(photoUrl, setPhoto);
   }, []);
 
-  const saveSighting = () => {
-    database.saveSighting(thisSighting);
+  const saveSighting = async (data: Sighting) => {
+    const sightingObject = new CatSightingObject(
+      docRef,
+      data.name || '',
+      data.info || '',
+      data.image || '',
+      data.fed,
+      data.health,
+      data.spotted_time || new Date(),
+      data.latitude,
+      data.longitude
+    );
+    database.saveSighting(sightingObject);
     router.push('/(app)/(tabs)');
   }
 
-  const deleteSighting = () => {
+  const deleteSighting = async () => {
     database.deleteSighting(photoUrl, docRef)
     router.push('/(app)/(tabs)');
-  };
-
-  const handleMapPress = (event: { nativeEvent: { coordinate: { latitude: any; longitude: any; }; }; }) => {
-    const { latitude, longitude } = event.nativeEvent.coordinate;
-    setLatitude(latitude);
-    setLongitude(longitude);
   };
 
   return (
@@ -61,58 +63,12 @@ const CatSightingScreen = () => {
       <Button style={buttonStyles.logoutButton} onPress={router.back}>
         <Ionicons name="arrow-back-outline" size={25} color="#fff" />
       </Button>
-      <Button style={buttonStyles.editButton} onPress={saveSighting}>
-        <Text style= {textStyles.editText}>Save</Text>
-      </Button>
-      <ScrollView contentContainerStyle={containerStyles.scrollView}>
-        <Text style={textStyles.catalogTitle}>
-          Edit A Cat Sighting
-        </Text>
-        <View style={containerStyles.container}>
-          {photoImage ? (<Image source={{ uri: photoImage }} style={containerStyles.catImage} resizeMode='contain'/>) : 
-            (<Text style={containerStyles.catImage}>Loading image...</Text>)}
-          <View style={containerStyles.inputContainer}>
-            <MapView
-              style={containerStyles.mapContainer}
-              initialRegion={{
-                latitude: 33.7756,
-                longitude: -84.3963,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01,
-              }}
-              onPress={handleMapPress} // This updates the location correctly
-            >
-              {location ? <Marker coordinate={location} /> : null}
-            </MapView>
-            <Text style={textStyles.sliderText}>Cat's Name</Text>
-            <TextInput
-              value={name}
-              onChangeText={setName}
-            />
-            <Text style={textStyles.sliderText}>Additional Info</Text>
-            <TextInput
-              value={info}
-              onChangeText={setInfo}
-            />
-            <Text style={textStyles.sliderText}>Date Sighted</Text>
-            <TextInput
-              value={date.toString()}
-              onChangeText={setInfo}
-            />
-            <View style={containerStyles.sliderContainer}>
-              <Switch value={health} onValueChange={setHealth}/>
-              <Text style={textStyles.sliderText}>Has been fed</Text>
-            </View>
-            <View style={containerStyles.sliderContainer}>
-              <Switch value={fed} onValueChange={setFed} />
-              <Text style={textStyles.sliderText}>Is in good health</Text>
-            </View>
-          </View>
-        </View>
-        <Button onPress={deleteSighting}>
-          Delete
-        </Button>
-      </ScrollView>
+      <SightingReportForm type="edit"
+        onSubmit={saveSighting}
+        onDelete={deleteSighting}
+        imageURL={photoImage}
+        defaultValues={thisSighting}
+      />
     </KeyboardAvoidingView>
   );
 };
