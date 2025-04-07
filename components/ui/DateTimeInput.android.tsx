ed6103bb744cbe5c386d7021466231ec4b4f6d28
Robert Gu <arcwandx86@gmail.com>
2025-04-07 15:25:24 -0400

    Fix datetimeinput for android

---
+++ components/ui/DateTimeInput.android.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+
+import { containerStyles, textStyles } from '@/styles';
+import { SetState } from '@/utils';
+
+type DateTimeInputProps = {
+  date: Date;
+  setDate: SetState<Date>;
+};
+
+export const DateTimeInput: React.FC<DateTimeInputProps> = ({
+  date,
+  setDate,
+}) => {
+  const [showPicker, setShowPicker] = useState<boolean>(false);
+
+  const onChange = (event: DateTimePickerEvent, selectedDate: Date| undefined) => {
+    if (selectedDate) {
+      setDate(selectedDate);
+    }
+    setShowPicker(false);
+  };
+
+  return (
+    <TouchableOpacity onPress={() => setShowPicker(true)}>
+      <View style={containerStyles.dateInput}>
+        <Text style={textStyles.dateText}>
+          {date ? date.toDateString() : 'Select Sighting Date'}
+        </Text>
+        {showPicker ? <DateTimePicker
+          value={date || new Date()}
+          mode="date"
+          display="default"
+          onChange={onChange}
+          style={containerStyles.picker}
+        /> : null}
+      </View>
+    </TouchableOpacity>
+  );
+};
