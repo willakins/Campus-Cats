127b7877c984d2e26a22bdf26e89513686fdf63f
Robert Gu <arcwandx86@gmail.com>
2025-04-10 01:19:59 -0400

    Set up frontend side of SSO auth flow

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,32 +1,7 @@
-import { Image, SafeAreaView, ScrollView, View } from 'react-native';
-import { useRouter } from 'expo-router';
-import { LoginForm } from '@/forms';
-import { useAuth } from '@/providers';
-import { containerStyles, globalStyles } from '@/styles';
+import { Redirect } from 'expo-router';
 
 const LoginScreen = () => {
-  const { login } = useAuth();
-  const router = useRouter();
-
-  const loginUser = async (username: string, password: string) => {
-    await login(username, password);
-    router.replace('/(app)/(tabs)');
-  };
-
-  return (
-    <SafeAreaView style={globalStyles.screen}>
-      <ScrollView contentContainerStyle={containerStyles.scrollViewCenter}
-  keyboardShouldPersistTaps="handled">
-        <Image source={require('@/assets/images/campus_cats_logo.png')} style={containerStyles.imageLarge}/>
-        <LoginForm
-          onSubmit={loginUser}
-          type="login"
-          onSwitchType={() => router.push('/whitelist')}
-          forgotPassword
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <Redirect href="/saml-sign-in" />
 };
 
 export default LoginScreen;
