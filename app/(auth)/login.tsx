ee51c7f1f2859ea9fe2f075e1f858c31435b7254
willakins <willakins23@gmail.com>
2025-04-10 14:10:40 -0400

    moving sso stuff back to its own page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Text, Image, View, SafeAreaView, ScrollView, Alert, TextInput } from 'react-native';
-import * as Linking from 'expo-linking';
-import * as WebBrowser from 'expo-web-browser';
-import { getAuth, SAMLAuthProvider, signInWithCredential } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Text, Image, View, SafeAreaView, ScrollView, TextInput } from 'react-native';
 import { useRouter } from 'expo-router';
 
 import { firebaseConfig } from '@/config/firebase';
 import { Button, SnackbarMessage } from '@/components';
 import { buttonStyles, containerStyles, globalStyles, textStyles } from '@/styles';
 import { useAuth } from '@/providers';
-import { fetchUser, mutateUser } from '@/models';
 
 const LoginScreen = () => {
   const router = useRouter();
-  const auth = getAuth();
   const { login } = useAuth();
-  const redirectUrl = Linking.createURL('/saml-sign-in');
-  const backendUrl = 'https://campuscats-d7a5e.firebaseapp.com/firebase-wrapper-app.html';
-  const [redirectData, setRedirectData] = useState<Linking.ParsedURL | null>(null);
-
   const [visible, setVisible] = useState<boolean>(false);
+
   const [formData, setFormData] = useState<{email: string; password: string;}>({email:"", password:""});
   const handleChange = (field: string, val: any) => {
     setFormData((prev: any) => ({ ...prev, [field]: val }));
@@ -34,55 +26,6 @@ const LoginScreen = () => {
       alert('Failed login. Consider using SSO.')
     } finally {
       setVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    const handleSSOSignIn = async () => {
-      if (redirectData?.queryParams?.credential) {
-        try {
-          setVisible(true);
-          const authCredential = SAMLAuthProvider.credentialFromJSON(
-            JSON.parse(redirectData.queryParams.credential as string)
-          );
-          const userCred = await signInWithCredential(auth, authCredential);
-          
-          // Fetch user data and set (necessary for tracking user.role)
-          const userData = await fetchUser(userCred.user.uid); 
-          mutateUser(userData);
-
-          router.replace('/(app)/(tabs)');
-        } catch (error) {
-          Alert.alert('SSO sign-in failed.');
-          console.error('SSO error:', error);
-        } finally {
-          setVisible(false);
-        }
-      }
-    };
-
-    handleSSOSignIn();
-  }, [redirectData]);
-
-  const _openAuthSessionAsync = async () => {
-    try {
-      const result = await WebBrowser.openAuthSessionAsync(
-        `${backendUrl}?linkingUri=${redirectUrl}&apiKey=${firebaseConfig.apiKey}&authDomain=${firebaseConfig.authDomain}`,
-        redirectUrl,
-        {
-          dismissButtonStyle: 'cancel',
-          enableDefaultShareMenuItem: false,
-        }
-      ) as WebBrowser.WebBrowserRedirectResult;
-
-      if (result?.url) {
-        setRedirectData(Linking.parse(result.url));
-      } else {
-        console.warn('SSO session dismissed or failed with type:', result?.type);
-      }
-    } catch (error) {
-      Alert.alert('SSO session error.');
-      console.error(error);
     }
   };
 
@@ -122,7 +65,7 @@ const LoginScreen = () => {
           <Button style={buttonStyles.mediumButton}onPress={loginWhitelistUser}>
             <Text style={textStyles.bigButtonText}>Sign in using Email</Text>
           </Button>
-          <Button style={buttonStyles.mediumButton} onPress={_openAuthSessionAsync}>
+          <Button style={buttonStyles.mediumButton} onPress={() => router.navigate('/saml-sign-in')}>
             <Text style={textStyles.bigButtonText}>Sign in using SSO</Text>
           </Button>
           <Button
