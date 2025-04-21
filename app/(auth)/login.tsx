c8c55f844ccb0698cb83ba87a5518d084b6c994c
Dragos-Lup <dlup3@gatech.edu>
2025-04-21 00:00:43 -0400

    oosps

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,8 +1,11 @@
-import { Image, SafeAreaView, ScrollView, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Image, View, SafeAreaView, ScrollView, TextInput } from 'react-native';
 import { useRouter } from 'expo-router';
-import { LoginForm } from '@/forms';
+
+import { firebaseConfig } from '@/config/firebase';
+import { Button, SnackbarMessage } from '@/components';
+import { buttonStyles, containerStyles, globalStyles, textStyles } from '@/styles';
 import { useAuth } from '@/providers';
-import { containerStyles, globalStyles } from '@/styles';
 
 import * as Notifications from 'expo-notifications';
 import * as Device from 'expo-device';
@@ -43,35 +46,76 @@ const savePushTokenToFirestore = async (uid: string, token: string) => {
 };
 
 const LoginScreen = () => {
-  const { login } = useAuth();
   const router = useRouter();
+  const { login } = useAuth();
+  const [visible, setVisible] = useState<boolean>(false);
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
 
-  const loginUser = async (username: string, password: string) => {
-    await login(username, password);
-
-    // Get the logged-in user
-    const currentUser = auth.currentUser;
-
-    if (currentUser) {
-      const token = await registerForPushNotificationsAsync();
-      if (token) {
-        await savePushTokenToFirestore(currentUser.uid, token);
-      }
-    }
-
-    router.replace('/(app)/(tabs)');
+  const [formData, setFormData] = useState<{email: string; password: string;}>({email:"", password:""});
+  const handleChange = (field: string, val: any) => {
+    setFormData((prev: any) => ({ ...prev, [field]: val }));
   };
 
+  const loginWhitelistUser = async () => {
+    try {
+      setVisible(true);
+      await login(formData.email, formData.password);
+      router.replace('/(app)/(tabs)');
+    } catch {
+      alert('Failed login. Consider using SSO.')
+    } finally {
+      setVisible(false);
+    }
+  };
+  
   return (
-    <SafeAreaView style={globalStyles.screen}>
-      <ScrollView contentContainerStyle={containerStyles.scrollViewCenter} keyboardShouldPersistTaps="handled">
-        <Image source={require('@/assets/images/campus_cats_logo.png')} style={containerStyles.imageLarge} />
-        <LoginForm
-          onSubmit={loginUser}
-          type="login"
-          onSwitchType={() => router.push('/whitelist')}
-          forgotPassword
-        />
+    <SafeAreaView style={containerStyles.wrapper}>
+       <SnackbarMessage text="Logging in..." visible={visible} setVisible={setVisible} />
+      <ScrollView
+        contentContainerStyle={containerStyles.scrollViewCenterPadded}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Image
+          source={require('@/assets/images/campus_cats_logo.png')}
+          style={containerStyles.imageLarge}
+          onLoad={() =>setImageLoaded(true)}
+        /> 
+        {!imageLoaded ? <View style={containerStyles.imageLarge}><Text style={textStyles.listTitle}>Loading...</Text></View>: null}
+        <View style={containerStyles.shadedCard}>
+          <Text style={textStyles.label}>Email</Text>
+          <View style={containerStyles.smallInputContainer}>
+            <TextInput 
+              value={formData.email || ''}
+              placeholder="email"
+              placeholderTextColor="#888"
+              onChangeText={(text) => handleChange('email', text)} 
+              style={textStyles.input}
+              multiline={false} />
+          </View>
+          <Text style={textStyles.label}>Password</Text>
+          <View style={containerStyles.smallInputContainer}>
+            <TextInput 
+              value={formData.password || ''}
+              placeholder="password"
+              placeholderTextColor="#888"
+              onChangeText={(text) => handleChange('password', text)} 
+              style={textStyles.input}
+              secureTextEntry={true}
+              multiline={false} />
+          </View>
+          <Button style={buttonStyles.mediumButton}onPress={loginWhitelistUser}>
+            <Text style={textStyles.bigButtonText}>Sign in using Email</Text>
+          </Button>
+          <Button style={buttonStyles.mediumButton} onPress={() => router.navigate('/saml-sign-in')}>
+            <Text style={textStyles.bigButtonText}>Sign in using SSO</Text>
+          </Button>
+          <Button
+            style={buttonStyles.mediumButton}
+            onPress={() => router.navigate('/whitelist')}
+          >
+            <Text style={textStyles.bigButtonText}>Apply For Whitelist</Text>
+          </Button>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
