615fa200a29091b617c11ab580b80bae03e1efd5
Robert Gu <arcwandx86@gmail.com>
2025-06-24 14:09:25 -0500

    Enable type checking information in ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,22 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    // 'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/strict',
     'expo',
     'prettier',
   ],
   rules: {
-    // '@typescript-eslint/explicit-function-return-type': 'error',
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
     '@typescript-eslint/return-await': 'error',
     '@typescript-eslint/consistent-type-assertions': 'error',
     '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-meaningless-void-operator': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: { attributes: false } },
+    ],
   },
   ignorePatterns: [
     '/dist/*',
